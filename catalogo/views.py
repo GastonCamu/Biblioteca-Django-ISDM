3d4945dfdbc3eadf1218f03f049bc3dbc5ad6266
Gastón Camú <camugaston2002@gmail.com>
2023-10-22 09:36:04 -0300

    Creacion del Login, Logout y Registro.

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
 
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'registration/register.html', {'form': CustomUserCreationForm})
+    
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            
+            user = authenticate(
+                username = form.cleaned_data['username'],
+                password = form.cleaned_data['password1']
+            )
+            login(request, user)
+            
+            return redirect('home')
+        else:
+            return render(request, 'registration/register.html', {"form": form})
+        
+@login_required
 def home(request):
-    return HttpResponse('<h1>¡Hola Mundo!</h1>')
-
+    context = {}
+    
+    return render(request, 'home/index.html', context)
 # Create your views here.
